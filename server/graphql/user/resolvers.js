676c6fff00db5fed5088756b05aaf1f44bcfe4bd
DenimCity <altidorj1@gmail.com>
2019-11-12 13:41:25 -0500

    created create and get quers

--- server/graphql/user/resolvers.js
+++ server/graphql/user/resolvers.js
@@ -1,10 +1,28 @@
+import { DB } from '../../entities';
+const { User } = DB;
+
 export const resolvers = {
 	Query: {
-		users: async (_, __, { DB: { User } }) => {
+		users: async () => {
 			try {
 				return User.find().sort({ createdAt: -1 });
 			} catch (error) {
 				console.log('TCL: error', error);
+			}
+		}
+	},
+	Mutation: {
+		createUser: async (_, { input }) => {
+			console.log('TCL: input', input);
+			try {
+				const response = await User.create({
+					...input,
+					createdAt: new Date().toISOString()
+				});
+				console.log('TCL: response', response);
+				return response;
+			} catch (error) {
+				console.log('TCL: error', error.message);
 			}
 		}
 	}
