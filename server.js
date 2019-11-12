372262f521bbfa2ab6c4086dbfc6a51bee127ed5
DenimCity <altidorj1@gmail.com>
2019-11-12 11:15:40 -0500

    setting up app entry point

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+import 'dotenv/config';
+
 import express from 'express';
+import startDBConnection from './server/database';
 
 const app = express();
+const PORT = process.env.PORT || 7000;
 
 app.get('/', (req, res) => {
 	res.send('Hlleo express');
 });
 
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	startDBConnection();
+});
